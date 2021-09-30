20e81f9fdb426f3a9051b5c5cb25c4173186c6ac
Maxence <raingeonneau.m@gmail.com>
2021-09-30 14:16:00 +0200

    ok

--- models/Skateparks.js
+++ models/Skateparks.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema({
   name: { type: String, unique: true },
   description: String,
+	adress: String,
   city: String,
-  adress: String,
   postalCode: Number,
-  userWhoCreate: Number,
 });
 
 module.exports = mongoose.model("Skateparks", schema);
